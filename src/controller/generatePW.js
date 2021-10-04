b1e347848a238da15e4bf6aa6c46ab477bba3310
Pham Van Khang <thanghayphacode@gmail.com>
2021-10-04 20:16:47 +0700

    add sonar

--- src/controller/generatePW.js
+++ src/controller/generatePW.js
@@ -4,7 +4,7 @@ const password = require("p4ssw0rd");
 
 const generatePW = (req, res) => {
   try {
-    const input = req.body.password;
+    const input = req.body.input;
     if (!input) return res.send("Invalid password");
     const output = password.hash(input, { cost: 5 });
     if (!output) return res.send("Invalid password hash");
