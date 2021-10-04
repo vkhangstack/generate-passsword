27c3a96e3ea81de4705c4412736828cf06c31239
Pham Van Khang <thanghayphacode@gmail.com>
2021-10-04 21:16:18 +0700

    add sonar

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require("cors");
 const generatePW = require("./routes/index");
 const expressSanitizer = require("express-sanitizer");
+const xXssProtection = require("x-xss-protection");
 
 app.disable("x-powered-by");
 app.use(
@@ -12,6 +13,7 @@ app.use(
     origin: process.env.BASE_URL,
   }),
 );
+app.use(xXssProtection());
 app.use(express.json());
 app.use(expressSanitizer());
 app.use("/", generatePW);
