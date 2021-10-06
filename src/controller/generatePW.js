10eda94f984320fe43e87b32dd9af66ed0d54a1a
Pham Van Khang <phamvankhang.tvi@gmail.com>
2021-10-06 22:52:28 +0700

    fix crypto agrith

--- src/controller/generatePW.js
+++ src/controller/generatePW.js
@@ -6,7 +6,7 @@ const generatePW = (req, res) => {
   try {
     const output = req.body.password;
     if (!output) return res.render("index", { data: "", output });
-    const data = crypto.createHash("sha256").update(output).digest("hex");
+    const data = crypto.createHash("sha512").update(output).digest("hex");
     if (!data) return res.render("index", { data: "", output });
     return res.render("index", { data, output });
   } catch (error) {
