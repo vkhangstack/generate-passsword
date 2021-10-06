7a3b1f767a68f266e9324515ad48c305cb4907a9
Pham Van Khang <phamvankhang.tvi@gmail.com>
2021-10-06 22:50:51 +0700

    fix require code

--- src/controller/generatePW.js
+++ src/controller/generatePW.js
@@ -5,10 +5,10 @@ const crypto = require("crypto");
 const generatePW = (req, res) => {
   try {
     const output = req.body.password;
-    if (!output) return res.render("index", { data: "", output: output });
+    if (!output) return res.render("index", { data: "", output });
     const data = crypto.createHash("sha256").update(output).digest("hex");
-    if (!data) return res.render("index", { data: "", output: output });
-    return res.render("index", { data: data, output: output });
+    if (!data) return res.render("index", { data: "", output });
+    return res.render("index", { data, output });
   } catch (error) {
     return res.status(400).send(error);
   }
